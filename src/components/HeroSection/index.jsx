05066a866f7c2146a3ce44a59004a571f970e13c
PabloJL <jup_jim_7@hotmail.com>
2024-02-06 13:41:34 -0600

    Setting Project

---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <section>
+      <h1 className="text-white mb-4 text-4xl ">Hello, I&apos;m Pablo</h1>
+      <p className="text-[#ADB7BE]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo iure ipsam
+        quod molestiae neque voluptatum.
+      </p>
+    </section>
+  );
+}
+
+export default HeroSection;
