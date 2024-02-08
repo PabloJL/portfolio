d494720fe9a06c81c473ed089c2f5e8f4c835fb5
PabloJL <jup_jim_7@hotmail.com>
2024-02-08 09:38:02 -0600

    First changes to about me section

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -13,8 +13,10 @@ function AboutSection() {
           className=" rounded-2xl"
         ></Image>
         <div>
-          <h2>About me</h2>
-          <p>
+          <h2 className="text-4xl font-bold text-white mb-4 mt-10 lg:mt-0">
+            About me
+          </h2>
+          <p className="text-base lg:text-lg">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Consequuntur, voluptate qui. Expedita natus ea sunt eligendi, quae
             non corrupti animi ullam dicta aperiam cumque, consequatur incidunt
@@ -23,6 +25,17 @@ function AboutSection() {
             molestiae sunt dolor obcaecati et temporibus perferendis pariatur,
             illo minima quos, placeat repellendus sed numquam optio.
           </p>
+          <div className="flex mt-8 gap-3">
+            <span className="font-semibold hover:text-white text-[#ADB7BE] border-b border-indigo-600">
+              Skills
+            </span>
+            <span className="font-semibold hover:text-white text-[#ADB7BE] border-b border-indigo-600">
+              Education
+            </span>
+            <span className="font-semibold hover:text-white text-[#ADB7BE] border-b border-indigo-600">
+              Experience
+            </span>
+          </div>
         </div>
       </div>
     </section>
