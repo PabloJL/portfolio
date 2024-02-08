f73596e9f2c087651ac361a9d71ccaaee2d7f7fa
PabloJL <jup_jim_7@hotmail.com>
2024-02-07 18:59:20 -0600

    Text animation added

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,12 +1,34 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+
 function HeroSection() {
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12 ">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            Hello, I&apos;m Pablo
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-violet-600 to-indigo-600 hover:bg-slate-200 text-white">
+              Hello, I&apos;m {""}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Pablo",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Web Developer",
+                1000,
+                "Software Developer",
+                1000,
+                "UI/UX Designer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo iure
@@ -21,15 +43,15 @@ function HeroSection() {
             </button>
           </div>
         </div>
-        <div className="col-span-5 place-content-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[390px] relative">
+        <div className="col-span-5 place-content-center mt-10 lg:mt-0 mx-auto lg:place-content-start text-center">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[390px] relative inline-block">
             <Image
               src="/images/dev.png"
               alt="developer image"
-              className="absolute tranform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={330}
               height={330}
-            ></Image>
+            />
           </div>
         </div>
       </div>
