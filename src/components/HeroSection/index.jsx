0547c51d48554e3b134b4689eb835948eb909958
PabloJL <jup_jim_7@hotmail.com>
2024-03-01 18:20:51 -0600

    Project post it added

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -24,7 +24,7 @@ function HeroSection() {
               sequence={[
                 // Same substring at the start will only be typed out once, initially
                 "Pablo",
-                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                1000,
                 "Web Developer",
                 1000,
                 "Software Developer",
